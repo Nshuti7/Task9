09e3da2b0d3121dde2975fa2364df041a597e1e5
Nshuti7 <bricekabanda8@gmail.com>
2025-04-01 15:36:24 +0300

    Commit #5
    In the NV added different methods to test different currencies

--- converter-module/src/test/java/coverter/ConvertTest.java
+++ converter-module/src/test/java/coverter/ConvertTest.java
@@ -1,10 +1,64 @@
 package coverter;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import rate.CurrencyRate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConvertTest {
+    private Convert converter;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
+        converter = new Convert();
+    }
+
+    @Test
+    void USDtoEUR_TEST1() {
+
+        int oldCurrncy = CurrencyRate.getUSD();
+        int newCurrncy = CurrencyRate.getEUR();
+        double amount = 100;
+
+        double actualResult = converter.convert(oldCurrncy, newCurrncy, amount);
+
+        double expectedResult = 85;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void EURDtoUSD_TEST2() {
+
+        int oldCurrncy = CurrencyRate.getEUR();
+        int newCurrncy = CurrencyRate.getUSD();
+        double amount = 50;
+
+        double actualResult = converter.convert(oldCurrncy, newCurrncy, amount);
+
+        double expectedResult = 59;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void USDtoUGX_TEST3() {
+        int oldCurrncy = CurrencyRate.getUSD();
+        int newCurrncy = CurrencyRate.getUGX();
+        double amount = 100;
+
+        double actualResult = converter.convert(oldCurrncy, newCurrncy, amount);
+        double expectedResult = 380000;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void UGXtoUSD_TEST4() {
+        int oldCurrncy = CurrencyRate.getUGX();
+        int newCurrncy = CurrencyRate.getUSD();
+        double amount = 250000;
+
+        double actualResult = converter.convert(oldCurrncy, newCurrncy, amount);
+        double expectedResult = 65.75;
+        assertEquals(expectedResult, actualResult);
     }
 }
